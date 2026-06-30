8821a085394c6935772709f25adbec71a1051df9
avi-gross <avigross2018@gmail.com>
2026-06-30 15:04:54 +0300

    file structure

---
+++ IntelligencePipeline/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntelligencePipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Classic World!");
+            
+        }
+    }
+}
